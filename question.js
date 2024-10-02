c5cf9fea1b8cbab1935bc5f52b7fe3fcfea5259d
AmourAnurag <anurag875636@gmail.com>
2024-10-02 16:10:02 +0530

    first commit

---
+++ question.js
@@ -0,0 +1,92 @@
+ export const Questions = [
+    {
+        Question: "1. which is the capital of India ",
+        Answer: [
+            { text: 'Lucknow', correct: false },
+            { text: 'Unnao', correct: false },
+            { text: 'Delhi', correct: true },
+            { text: 'Mumbai', correct: false },
+        ]
+    },
+    {
+        Question: "2. which is the capital of uttar Pradesh ",
+        Answer: [
+            { text: 'Lucknow', correct: true },
+            { text: 'Unnao', correct: false },
+            { text: 'Farrukhabad', correct: false },
+            { text: 'Noida', correct: false },
+        ]
+    },
+    {
+        Question: "3. which is the capital of Madhya Pradesh ",
+        Answer: [
+            { text: 'Gwalior', correct: false },
+            { text: 'Bhopal', correct: true },
+            { text: 'Farrukhabad', correct: false },
+            { text: 'Gya', correct: false },
+        ]
+    },
+    {
+        Question: "4. Total state in India ",
+        Answer: [
+            { text: '25', correct: false },
+            { text: '28', correct: true },
+            { text: '27', correct: false },
+            { text: '26', correct: false },
+        ]
+    },
+    {
+        Question: "5. which is the National bird of  India ",
+        Answer: [
+            { text: 'Peacock', correct: true },
+            { text: 'Parrot', correct: false },
+            { text: 'Crow', correct: false },
+            { text: 'Hen', correct: false },
+        ]
+    },
+    {
+        Question: "6. What is the national animal of india ",
+        Answer: [
+            { text: 'buffalo', correct: false },
+            { text: 'cow', correct: true },
+            { text: 'Unknown', correct: false },
+            { text: 'Goat', correct: false },
+        ]
+    },
+    {
+        Question: "7. What is the national sweet of india ",
+        Answer: [
+            { text: 'Soan Papdi', correct: false },
+            { text: 'Sugar', correct:false  },
+            { text: 'Jalebi', correct: true },
+            { text: 'Kaju Katli', correct: false },
+        ]
+    },
+    {
+        Question: "8. What is the national fruit of india ",
+        Answer: [
+            { text: 'Mango', correct: true },
+            { text: 'Grapes', correct: false },
+            { text: 'Guava', correct: false },
+            { text: 'Nut', correct: false },
+        ]
+    },
+    {
+        Question: "9. What is the national sign of india ",
+        Answer: [
+            { text: 'Taj mahal', correct: false },
+            { text: 'Lal kila', correct: false },
+            { text: 'Ashok Stambh', correct: true },
+            { text: 'Gya', correct: false },
+        ]
+    },
+    {
+        Question: "10. What is the national song of india ",
+        Answer: [
+            { text: 'Vande Matram', correct: true },
+            { text: 'Jan Gan Man', correct: false },
+            { text: 'Keshriya', correct: false },
+            { text: 'A antthm', correct: false },
+        ]
+    }
+];
---
+++ script.js
@@ -0,0 +1,107 @@
+import { Questions } from "./question.js";
+
+console.log(Questions);  
+console.log(Questions[0].Question);  
+
+
+
+
+let index=0
+let number=0
+let i=0
+let result=0;
+let question = document.getElementById("Question")
+let solveQuestion = document.getElementById("solve")
+let answerbtn = document.getElementById("answerButton")
+let btn = document.getElementsByClassName("btn")
+let nextbutton = document.getElementById("nxt")
+let StartButton = document.getElementById("start")
+let b1 = document.getElementById("b1")
+let b2 = document.getElementById("b2")
+let b3 = document.getElementById("b3")
+let b4 = document.getElementById("b4")
+//Start quiz
+StartButton.addEventListener("click", showQuestion)
+b1.addEventListener("click", check)
+b2.addEventListener("click", check)
+b3.addEventListener("click", check)
+b4.addEventListener("click", check)
+function check(e) {
+    if (i < 1) {
+       let l = e.target.value
+        if (l === "true") {
+            e.target.style.backgroundColor = "#12e31275"
+            
+            result += 1;
+        } else {
+            e.target.style.backgroundColor = "rgb(227 35 18 / 55%)"
+        }
+        i = +1
+    } else {
+        nextQuestion();
+
+    }
+
+}
+//FNCTION FOR SHOWING QUESTION
+let Quizbutton = document.getElementById("quiz")
+let ScoreButton = document.getElementById("score")
+function showQuestion() {
+    console.log("called");
+    solveQuestion.innerHTML=index
+    Quizbutton.style.display = "flex"
+    ScoreButton.style.display = "none"
+    nextbutton.style.display = "block"
+    StartButton.style.display = "none"
+    if (index > 9) {
+        SCORE()
+        result = 0
+        index = 0
+        question.innerHTML = Questions[index].Question;
+        answerbtn = Questions[index].Answer.forEach((item) => {
+
+            /* answerbtn.innerHTML=item.text */
+            btn[number].innerHTML = item.text
+            number += 1
+        });
+    }
+
+    else {
+
+        nextbutton.innerHTML = "Next"
+        question.innerHTML = Questions[index].Question;
+        answerbtn = Questions[index].Answer.forEach((item) => {
+
+
+            btn[number].innerHTML = item.text
+            btn[number].value = item.correct
+            number += 1
+
+        });
+    }
+
+
+}
+nextbutton.addEventListener("click", nextQuestion)
+function nextQuestion() {
+    Restart()
+    index += 1
+    i = 0
+    number = 0
+    showQuestion()
+}
+function Restart() {
+
+    btn.b1.style.backgroundColor = "white"
+    btn.b2.style.backgroundColor = "white"
+    btn.b3.style.backgroundColor = "white"
+    btn.b4.style.backgroundColor = "white"
+}
+function SCORE() {
+    ScoreButton.style.display = "flex"
+    nextbutton.style.display = "none"
+    StartButton.style.display = "block"
+    ScoreButton.innerHTML = `Score = ${result}`
+    Quizbutton.style.display = "none"
+
+}
