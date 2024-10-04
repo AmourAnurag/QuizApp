1a8e5ba22892b37132646a18189b200bd676df36
AmourAnurag <anurag875636@gmail.com>
2024-10-04 13:45:50 +0530

    coomt

--- script.js
+++ script.js
@@ -8,6 +8,9 @@ let number = 0;
 let i = 0;
 let result = 0;
 let question = document.getElementById("Question");
+let Img1 = document.getElementById("img1");
+let Img3 = document.getElementById("img3");
+let Img2 = document.getElementById("img2");
 let Time = document.getElementById("time");
 let solveQuestion = document.getElementById("solve");
 let answerbtn = document.getElementById("answerButton");
@@ -24,14 +27,22 @@ b1.addEventListener("click", check);
 b2.addEventListener("click", check);
 b3.addEventListener("click", check);
 b4.addEventListener("click", check);
+Img1.style.display = "none";
+Img2.style.display = "none";
+Img3.style.display = "none";
 function check(e) {
   if (i < 1) {
     let l = e.target.value;
     if (l === "true") {
+
+      Img3.style.display = "none";
+      Img1.style.display = "inline-block";
       e.target.style.backgroundColor = "#12e31275";
 
       result += 1;
     } else {
+      Img3.style.display = "none";
+      Img2.style.display = "inline-block";
       e.target.style.backgroundColor = "rgb(227 35 18 / 55%)";
     }
     i = +1;
@@ -43,6 +54,15 @@ function check(e) {
 let Quizbutton = document.getElementById("quiz");
 let ScoreButton = document.getElementById("score");
 function showQuestion() {
+  Img1.style.display = "none";
+  Img3.style.display = "inline-block";
+  Img2.style.display = "none";
+  Time.style.display = "inline-block";
+  if (index == 0)
+    alert(
+      "You have 30 seconds to solve question each question carry 1 mark No negative Marking If you do not know answer then skip the question"
+    );
+
   solveQuestion.innerHTML = index;
   Quizbutton.style.display = "flex";
   ScoreButton.style.display = "none";
@@ -75,12 +95,12 @@ function startCountdown(seconds) {
   Time.innerHTML = timeRemaining; // Display the initial time
   //clear the existing time
   clearInterval(timer);
-   timer = setInterval(() => {
+  timer = setInterval(() => {
     timeRemaining--;
     Time.innerHTML = timeRemaining; // Update the UI
 
     if (timeRemaining <= 0) {
-      clearInterval(countdown); // Stop the countdown
+      clearInterval(timer); // Stop the countdown
       // Enable answer buttons after countdown
       Array.from(btn).forEach((button) => {
         button.disabled = false;
@@ -106,6 +126,7 @@ function Restart() {
   btn.b4.style.backgroundColor = "white";
 }
 function SCORE() {
+  Time.style.display = "none";
   ScoreButton.style.display = "flex";
   nextbutton.style.display = "none";
   StartButton.style.display = "block";
